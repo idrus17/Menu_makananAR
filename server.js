ac4ddf40ffa7d5cb366518c4f167321d524aa7c9
idrus17 <mdrus143@gmail.com>
2022-06-13 14:55:53 +0700

    test

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('scan');
 });
 
 app.listen(8080, () => {
